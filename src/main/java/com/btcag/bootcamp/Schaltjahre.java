c9535305565cfa1b99be78f6e575da2ba21b9eb6
Schumacher <colin.schumacher@btc-ag.com>
2024-09-25 13:46:24 +0200

    25.09.

--- src/main/java/com/btcag/bootcamp/Schaltjahre.java
+++ src/main/java/com/btcag/bootcamp/Schaltjahre.java
@@ -7,6 +7,14 @@ public class Schaltjahre {
         Scanner scanner = new Scanner(System.in);
         int jahr = scanner.nextInt();
 
-
+        if (jahr % 4 == 0) {
+            if (jahr % 100 == 0 && jahr % 400 != 0) {
+                System.out.println(jahr + " ist kein Schaltjahr.");
+            } else {
+                System.out.println(jahr + " ist ein Schaltjahr.");
+            }
+        } else {
+            System.out.println(jahr + " ist kein Schaltjahr.");
+        }
     }
 }
