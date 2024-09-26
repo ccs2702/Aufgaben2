6b60c66757905d17ecf71d913b7d65717525d954
Schumacher <colin.schumacher@btc-ag.com>
2024-09-26 08:27:56 +0200

    26.09.

--- src/main/java/com/btcag/bootcamp/Sortieren.java
+++ src/main/java/com/btcag/bootcamp/Sortieren.java
@@ -3,5 +3,12 @@ package com.btcag.bootcamp;
 public class Sortieren {
     public static void main(String[] args) {
 
+        String[] stringArray = {"1", "5", "3", "19", "8"};
+
+        System.out.println(stringArray[0]);
+        System.out.println(stringArray[1]);
+        System.out.println(stringArray[2]);
+        System.out.println(stringArray[3]);
+        System.out.println(stringArray[4]);
     }
 }
